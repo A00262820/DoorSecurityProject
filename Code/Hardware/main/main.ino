50d0a32c795e0ff69e77da935cdda3fbaa2808eb
jag <a00262820@mycambrian.ca>
2025-04-12 08:49:52 +0530

    frame capture issue rectification

--- Code/Hardware/main/main.ino
+++ Code/Hardware/main/main.ino
@@ -98,14 +98,14 @@ void setupCamera() {
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y9_GPIO_NUM;
-  config.pin_d1 = Y8_GPIO_NUM;
-  config.pin_d2 = Y7_GPIO_NUM;
-  config.pin_d3 = Y6_GPIO_NUM;
-  config.pin_d4 = Y5_GPIO_NUM;
-  config.pin_d5 = Y4_GPIO_NUM;
-  config.pin_d6 = Y3_GPIO_NUM;
-  config.pin_d7 = Y2_GPIO_NUM;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
   config.pin_xclk = XCLK_GPIO_NUM;
   config.pin_pclk = PCLK_GPIO_NUM;
   config.pin_vsync = VSYNC_GPIO_NUM;
@@ -116,6 +116,9 @@ void setupCamera() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG; // Adjust format as needed
+
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
 
   // Adjust frame size for balance between speed and detail
   config.frame_size = FRAMESIZE_QVGA; // Example: QVGA (320x240)
